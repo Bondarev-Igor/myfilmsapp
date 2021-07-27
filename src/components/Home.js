62d67d1d0b969bec0e05020146037024a3ab99e5
Evgeniy Nesterchuk <e.nesterchuk@belitsoft.com>
2021-07-27 17:47:15 +0300

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
       {state.results[0]?
       <HeroImage
         image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-        title={state.results[0].original.title}
+        title={state.results[0].original_title}
         text={state.results[0].overview}
       /> 
       : null
