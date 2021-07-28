b74030ce6f69cfa8ae7e9ee88f7aa265eb4d9d61
Evgeniy Nesterchuk <e.nesterchuk@belitsoft.com>
2021-07-28 15:17:57 +0300

    create Thumb, add Thumb in Home, add style to Grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 // Componentas
 import HeroImage from './HeroImage/HeroImage';
 import Grid from './Grid/Grid';
+import Thumb from './Thumb/Thumb';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -29,9 +30,17 @@ const Home = () => {
       }
       <Grid header='Popular Movies'>
         {state.results.map(movie => (
-          <div key = {movie.id}>{movie.title}</div>
+          <Thumb 
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
         ))}
-
       </Grid>
     
     </>
