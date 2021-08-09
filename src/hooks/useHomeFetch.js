fe516b793f81ae53063a5238e377282da40a19fd
Evgeniy Nesterchuk <e.nesterchuk@belitsoft.com>
2021-08-09 17:07:29 +0300

    add sessionStorage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 //API
 import API from '../API';
+//Helpers
+import { isPersistedState } from '../helpers'
+
 
 const initialState  = {
 	page: 0,
@@ -37,6 +40,15 @@ export const useHomeFetch = () => {
 
   // Initial and search
     useEffect (() => {
+      if (!searchTerm) {
+        const sessionState = isPersistedState('homeState');
+        console.log('Grabbing from sessionStorage');
+        if(sessionState) {
+          setState(sessionState);
+          return;
+        }
+      }
+      console.log('Grabbing from API');
       setState(initialState);      
       fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -50,6 +62,11 @@ export const useHomeFetch = () => {
     setIsLoadingMore(false);
 
   }, [isLoadingMore, searchTerm, state.page]);
+
+  //Write to sessionStorage
+  useEffect(() => {
+    if(!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state))
+  }, [searchTerm, state])
 
 
   return { state, loading, error, searchTerm,  setSearchTerm, setIsLoadingMore };
